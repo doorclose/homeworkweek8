72954e3ccf279554c717a23e1982e59a69649f4e
doorclose <codebuster2023@gmail.com>
2024-04-21 22:43:30 +0100

    My second push to repo.

--- Week8HW/src/inheritence1/Codebuster.java
+++ Week8HW/src/inheritence1/Codebuster.java
@@ -1,5 +1,7 @@
 package inheritence1;
-
+//Write a Java program to create a class called codebuster with a method called
+//groupName(). Create a subclass called codebusterGroups() that overrides the
+//groupName() method.
 public class Codebuster {
 
     public void groupName(){
