e6d88a1bb1e40400d26079c487aefe6f7cbd1248
doorclose <codebuster2023@gmail.com>
2024-04-21 22:42:18 +0100

    My first push to repo.

---
+++ Week8HW/src/inheritance2/Vehical.java
@@ -0,0 +1,10 @@
+package inheritance2;
+//Write a Java program to create a class called Vehicle with a method called drive().
+//Create a subclass called Car that overrides the drive() method to print "Repairing a
+//car".
+public class Vehical {
+
+    public void drive(){
+        System.out.println("don't drive fast");
+    }
+}
