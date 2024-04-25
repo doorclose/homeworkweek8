21fd6bf1715f4c8054cc508b32add956f701a935
doorclose <codebuster2023@gmail.com>
2024-04-25 22:54:54 +0100

    My first push to repo.

---
+++ Week8HW/src/projecthw8/TringleImageQue15.java
@@ -0,0 +1,18 @@
+package projecthw8;
+
+public class TringleImageQue15 {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
